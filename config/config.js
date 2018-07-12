dc54f939863371a033e2e4a8728880d1bb030169
Fabio <fa.espinosa10@uniandes.edu.co>
2018-07-12 16:02:21 +0200

    add logging

--- config/config.js
+++ config/config.js
@@ -19,3 +19,5 @@ exports.root_url_prefix =
     config[process.env.NODE_ENV || 'development']['root_url_prefix'];
 
 exports.api_url = config[process.env.NODE_ENV || 'development']['api_url'];
+
+console.log(config[process.env.NODE_ENV || 'development']);
