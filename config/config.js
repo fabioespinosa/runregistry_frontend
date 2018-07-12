f1df665d6531a33c8d188f9e6f7be4ba6025ddf7
Fabio <fa.espinosa10@uniandes.edu.co>
2018-07-12 16:54:31 +0200

    remove logging from node_env

--- config/config.js
+++ config/config.js
@@ -15,5 +15,3 @@ exports.root_url_prefix =
     config[process.env.NODE_ENV || 'development']['root_url_prefix'];
 
 exports.api_url = config[process.env.NODE_ENV || 'development']['api_url'];
-
-console.log(config[process.env.NODE_ENV || 'development']);
