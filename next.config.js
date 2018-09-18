182f8712f8624f71c05ad30a4f97279f34585529
Fabio <fa.espinosa10@uniandes.edu.co>
2018-09-18 14:02:42 +0200

    fix .babelrc in next.config.js

--- next.config.js
+++ next.config.js
@@ -9,10 +9,21 @@ if (typeof require !== 'undefined') {
 
 module.exports = withCss({
     assetPrefix: root_url_prefix, // affects page bundles and app/commons/vendor scripts
-    webpack: config => {
+    webpack: (config, { defaultLoaders }) => {
         config.output.publicPath = `/${root_url_prefix}${
             config.output.publicPath
         }`; // affects 'chunks'
+        defaultLoaders.babel.options.plugins.push([
+            'transform-decorators-legacy',
+            [
+                'import',
+                {
+                    libraryName: 'antd',
+                    style: 'css'
+                }
+            ]
+        ]);
+
         return config;
     }
 });
